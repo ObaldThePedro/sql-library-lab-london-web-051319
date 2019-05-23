5f9a29e954c6a1a84918a4c9b30e02a0597a5caa
0baldThePedro <pedro.nunes.loureiro@gmail.com>
2019-05-23 14:13:52 +0100

    Done.

---
+++ lib/schema.sql
@@ -0,0 +1,48 @@
+CREATE TABLE series (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    subgenre_id INTEGER,
+    author_id INTEGER,
+    FOREIGN KEY(subgenre_id) REFERENCES subgenres(id),
+    FOREIGN KEY(author_id) REFERENCES authors(id)
+
+);
+
+CREATE TABLE subgenres(
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE authors(
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+CREATE TABLE books(
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    year INTEGER,
+    series_id INTEGER,
+    FOREIGN KEY(series_id) REFERENCES series(id)
+);
+
+CREATE TABLE characters(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    motto TEXT,
+    species TEXT,
+    author_id INTEGER,
+    series_id INTEGER,
+    FOREIGN KEY(author_id) REFERENCES authors(id),
+    FOREIGN KEY(series_id) REFERENCES series(id)
+);
+
+CREATE TABLE character_books(
+    id INTEGER PRIMARY KEY,
+    author_id INTEGER,
+    character_id INTEGER,
+    book_id INTEGER,
+    FOREIGN KEY(author_id) REFERENCES authors(id),
+    FOREIGN KEY(book_id) REFERENCES books(id),
+    FOREIGN KEY(character_id) REFERENCES characters(id)
+);
